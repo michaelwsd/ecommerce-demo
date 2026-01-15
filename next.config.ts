0ba6d9ed545248f2283abfa1345039af6827fcfc
michaelwsd <a0416517630@icloud.com>
2026-01-15 22:10:16 +1100

    fixed image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["mtvthvlzjhgvkglwhuih.supabase.co"],
+  },
 };
 
 export default nextConfig;
